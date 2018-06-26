2bf5d5cc2fb4305794095bd3268ec41a810858fd
Carl Dawson <carl@invisible-gorilla.com>
2018-06-26 06:48:28 -0400

    Added /GET quotes/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const {ObjectID} = require('mongodb');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 let {mongoose} = require('./db/mongoose');
 let {Quote} = require('./models/quote');
@@ -10,6 +12,7 @@ let app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('hello world');
@@ -22,11 +25,31 @@ app.post('/quotes', (req, res) => {
     description: req.body.description
   });
 
-  quote.save().then(doc => {
-    res.send(doc);
+  quote.save().then(quote => {
+    res.send(quote);
   }, e => {
     res.status(400).send(e);
   })
+});
+
+app.get('/quotes/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Quote.findOne({
+    _id: id
+  }).then(quote => {
+    if (!quote) {
+      return res.status(404).send();
+    }
+
+    res.send(quote);
+  }).catch(e => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => console.log(`Started on port ${port}`));
